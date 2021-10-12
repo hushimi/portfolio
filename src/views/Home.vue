b41396898ec5a37187035c24da33e3bf026a5034
kosuke <mowmow.meganekun@gmail.com>
2021-10-12 23:18:54 +0900

    fix header style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,9 +4,9 @@
             <div class="w-11/12 h-11/12 message glass two-cols">
                 <!-- アイコンとメッセージ -->
                 <div class="h-auto table">
-                    <div class="table-cell align-middle m-auto text-center">
+                    <div class="table-cell align-middle h-full w-full text-center">
                         <img src="@/assets/img/logo.png" class="m-auto">
-                        <p class="break-words font-bold">
+                        <p class="break-words font-bold text-center">
                             I am <span>A</span> self taught programmer
                         </p>
                     </div>
@@ -14,7 +14,7 @@
 
                 <!-- スキルテキストアニメーション -->
                 <div class="table">
-                    <div class="table-cell align-middle m-auto text-center">
+                    <div class="table-cell align-middle h-full w-full text-center">
                         <p>Front End</p>
                         <p>Back End</p>
                         <p>App Development</p>
